5c1d0c20999783f05cc91c837fdde08bbed3b766
y-web21 <y.web2019@gmail.com>
2021-03-19 14:56:56 +0900

    メール認証済みでなくてもログイン機能が使えたバグを修正

--- app/Http/Controllers/PosterPagesController.php
+++ app/Http/Controllers/PosterPagesController.php
@@ -18,6 +18,7 @@ class PosterPagesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('verified');
     }
 
     /**
--- app/Http/Controllers/UploadImagesController.php
+++ app/Http/Controllers/UploadImagesController.php
@@ -14,6 +14,7 @@ class UploadImagesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('verified');
     }
 
     /**
