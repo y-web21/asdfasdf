020a4c114dbb493e9773b2547be24e8f7164a666
y-web <y.web.2019@gmail.com>
2021-03-26 19:02:54 +0900

    update yml

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -19,7 +19,7 @@ class ExampleTest extends TestCase
         $this->withoutExceptionHandling();
         var_dump($dir);
         var_dump($path);
-        mkdir($dir, 0664, true);
+        mkdir($dir, 0666, true);
         file_put_contents($path ,'content', LOCK_EX | FILE_APPEND);
         // file_get_contents($path);
         // $response = $this->get('/');
